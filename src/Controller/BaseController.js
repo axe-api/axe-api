3716f28b790690529a28ea4ba50ed9ef72e32cc5
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-05-09 12:38:39 +0300

    Creating a record has been completed.

--- src/Controller/BaseController.js
+++ src/Controller/BaseController.js
@@ -1,3 +1,5 @@
+import { getFormData } from "./Helper.js";
+
 class BaseController {
   async paginate({
     request,
@@ -8,9 +10,8 @@ class BaseController {
     Database,
     Logger,
   }) {
-    const result = await Database.select(["id", "name", "email"])
+    const result = await Database.select("*")
       .from(model.instance.table)
-      .where("id", ">", 100)
       .limit(5);
 
     response.json(result);
@@ -25,11 +26,56 @@ class BaseController {
   }
 
   async store({ request, response, model, parentModel, Config, Database }) {
-    response.json({
-      name: "store",
-      model,
-      parentModel,
-    });
+    // We should validate the data
+    // await this.validation.validate(
+    //   request.method(),
+    //   request.all(),
+    //   Model.validations
+    // );
+
+    // Preparing the data
+    const data = getFormData(request, model.instance.fillable);
+    console.log(data);
+
+    // // Binding parent id if there is.
+    // if (request.adonisx.parent_column) {
+    //   data[snakeCase(request.adonisx.parent_column)] =
+    //     params[request.adonisx.parent_column];
+    // }
+
+    // // We should call onBeforeCreate action
+    // const modelName = modelPath.replace("App/Models/", "");
+
+    // await this.repositoryHelper.callAction(
+    //   request.adonisx.url,
+    //   "onBeforeCreate",
+    //   { request, params, data }
+    // );
+
+    // this.event.fire(`onBeforeCreate${modelName}`, { request, params, data });
+
+    // // Creating the item
+    const [insertId] = await Database(model.instance.table).insert(data);
+    const item = await Database(model.instance.table)
+      .where("id", insertId)
+      .first();
+
+    // // We should call onAfterCreate action
+    // await this.repositoryHelper.callAction(
+    //   request.adonisx.url,
+    //   "onAfterCreate",
+    //   { request, params, data, item }
+    // );
+
+    // this.event.fire(`onAfterCreate${modelName}`, {
+    //   request,
+    //   params,
+    //   data,
+    //   item,
+    // });
+
+    // Returning response
+    response.json(item);
   }
 
   async update({ request, response, model, parentModel, Config, Database }) {
---
+++ src/Controller/Helper.js
@@ -0,0 +1,14 @@
+export const getFormData = (request, fillable) => {
+  let fields = fillable;
+  if (!Array.isArray(fillable)) {
+    fields = fillable[request.method];
+  }
+
+  const filtered = {};
+
+  for (const field of fields) {
+    filtered[field] = request.body[field] ? request.body[field] : null;
+  }
+
+  return filtered;
+};
