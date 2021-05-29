61db756f829eaefb95253a1921d320e0a823ac7c
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2021-05-29 17:39:54 +0300

    Fixed #28

---
+++ src/Resolvers/detectors/sqliteDetector.js
@@ -0,0 +1,31 @@
+export default async ({ knex, schema }) => {
+  const tables = (await knex.table("sqlite_master").where("type", "table")).map(
+    (item) => item.tbl_name
+  );
+
+  const columns = [];
+
+  for (const table of tables) {
+    const result = await knex.raw(`
+      select * from pragma_table_info("${table}")
+    `);
+    columns.push(
+      ...result.map((i) => {
+        return {
+          name: i.name,
+          tableName: table,
+          isNullable: i.notnull === 0,
+          dataType: i.type,
+          defaultValue: i.dflt_value,
+          maxLength: null,
+          numericPrecision: null,
+          numericScale: null,
+          isPrimary: i.pk === 1,
+          isAutoIncrement: null,
+        };
+      })
+    );
+  }
+
+  return columns;
+};
