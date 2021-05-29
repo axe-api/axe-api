be5402b48e9d2a230d243459807efec3a01dfcda
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2021-05-29 19:29:08 +0300

    Fixed sonarqube bugs

--- src/Resolvers/RelationResolver.js
+++ src/Resolvers/RelationResolver.js
@@ -6,7 +6,9 @@ const getMethods = (obj) => {
   let properties = new Set();
   let currentObj = obj;
   do {
-    Object.getOwnPropertyNames(currentObj).map((item) => properties.add(item));
+    Object.getOwnPropertyNames(currentObj).forEach((item) =>
+      properties.add(item)
+    );
   } while ((currentObj = Object.getPrototypeOf(currentObj)));
   return [...properties.keys()].filter(
     (item) => typeof obj[item] === "function"
