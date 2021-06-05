0cc7cd41cedba381ade749ffafecbfbf2e89c365
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2021-06-05 22:09:42 +0300

    Base Model has been moved to the core directory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Server from "./src/Server.js";
-import Model from "./src/Models/Model.js";
-import IoC from "./src/Core/IoC.js";
+import Model from "./src/core/Model.js";
+import IoC from "./src/core/IoC.js";
 import { LOG_LEVEL, HOOK_FUNCTIONS, HANDLERS } from "./src/Constants.js";
 import HttpResponse from "./src/core/HttpResponse.js";
 
