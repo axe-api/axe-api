9bcd2ffcee94f68ba90ac90a820d817243d1e01f
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2021-07-25 13:36:47 +0300

    Fixed #26

--- tests/integrations/scenarios/app/Config/Database.js
+++ tests/integrations/scenarios/app/Config/Database.js
@@ -6,6 +6,7 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
+    searchPath: [process.env.DB_USER, "public"],
   },
   pool: {
     min: 2,
--- tests/integrations/scenarios/tests/helper.js
+++ tests/integrations/scenarios/tests/helper.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import request from "supertest";
 import mysql from "mysql";
+import { Pool } from "pg";
 
 export const get = async ({ url, status }) => {
   return await request("localhost:3000")
@@ -41,22 +42,41 @@ const truncateMySQL = async (table) => {
     port: process.env.DB_PORT,
   });
 
-  connection.connect(function (err) {
-    if (err) throw err;
-    connection.query("SET FOREIGN_KEY_CHECKS = 0;", function (err) {
+  return new Promise((resolve) => {
+    connection.connect(function (err) {
       if (err) throw err;
-      const sql = `TRUNCATE TABLE ${table}`; //NOSONAR
-      connection.query(sql, function (err) {
+      connection.query("SET FOREIGN_KEY_CHECKS = 0;", function (err) {
         if (err) throw err;
+        const sql = `TRUNCATE TABLE ${table}`; //NOSONAR
+        connection.query(sql, function (err) {
+          if (err) throw err;
+          resolve();
+        });
       });
     });
   });
+};
+
+const truncatePostgres = async (table) => {
+  const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    searchPath: [process.env.DB_USER, "public"],
+  });
+  const sql = `TRUNCATE TABLE ${table} CASCADE`; //NOSONAR
+  await pool.query(sql);
+  await pool.end();
 };
 
 export const truncate = async (table) => {
   switch (process.env.DB_CLIENT) {
     case "mysql":
       return await truncateMySQL(table);
+    case "postgres":
+      return await truncatePostgres(table);
     default:
       throw new Error(`Unknown DB client: ${process.env.DB_CLIENT}`);
   }
