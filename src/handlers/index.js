3b5b8a44eeedb62d90f133a3408a5f3747f6198f
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-10-03 13:55:47 +0300

    Fixed #87

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,7 +1,8 @@
+import autosave from "./autosave.js";
 import store from "./store.js";
 import show from "./show.js";
 import paginate from "./paginate.js";
 import update from "./update.js";
 import destroy from "./destroy.js";
 
-export default { store, show, paginate, update, destroy };
+export default { autosave, store, show, paginate, update, destroy };
