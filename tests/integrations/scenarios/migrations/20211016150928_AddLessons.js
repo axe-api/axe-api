5f905e9dc8d953947b9d81287501b83fc166bbf4
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2021-10-17 20:02:41 +0300

    Fixed wrong test items

--- tests/integrations/scenarios/migrations/20211016150928_AddLessons.js
+++ tests/integrations/scenarios/migrations/20211016150928_AddLessons.js
@@ -1,10 +1,8 @@
 export const up = function (knex) {
   const items = [
-    { name: "AYT Matematik" },
-    { name: "TYT Matematik" },
-    { name: "Fizik" },
-    { name: "Kimya" },
-    { name: "Geometri" },
+    { name: "Math" },
+    { name: "Computer Science" },
+    { name: "Network" },
   ];
 
   return knex.table("lessons").insert(items);
