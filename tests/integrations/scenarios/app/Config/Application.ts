e60b494c384919ba537a495195c394626c543e82
Ozgur Adem ISIKLI <ozgur@shape.dk>
2022-12-24 16:58:14 +0100

    Fixed some bugs

--- tests/integrations/scenarios/app/Config/Application.ts
+++ tests/integrations/scenarios/app/Config/Application.ts
@@ -3,7 +3,7 @@ import { IApplicationConfig, LogLevels } from "axe-api";
 const config: IApplicationConfig = {
   prefix: "api",
   env: process.env.NODE_ENV || "production",
-  port: process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 5000,
+  port: process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000,
   logLevel: LogLevels.INFO,
   transaction: [],
   serializers: [],
