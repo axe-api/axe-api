e95e334a1deba2e11ef9201bdf691cc2b165fa30
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-09-09 14:56:21 +0200

    Merged with master

--- tests/integrations/serve-dev.js
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import { Server } from "axe-api";
-
-const appFolder = path.join(path.resolve(), "scenarios", "app");
-const server = new Server(appFolder);
-server.listen();
