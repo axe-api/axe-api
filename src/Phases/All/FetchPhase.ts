b76b3e2e0f7d42f41a6fff294be190c9361fede4
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-01-15 21:50:46 +0100

    Fixed #330

--- src/Phases/All/FetchPhase.ts
+++ src/Phases/All/FetchPhase.ts
@@ -1,7 +1,25 @@
+import { addForeignKeyQuery } from "../../Handlers/Helpers";
 import { IContext } from "../../Interfaces";
 
 export default async (context: IContext) => {
-  if (context.query) {
+  if (context.query && context.queryParser && context.conditions) {
+    // Users should be able to select some fields to show.
+    context.queryParser.applyFields(context.query, context.conditions.fields);
+
+    // Binding parent id if there is.
+    addForeignKeyQuery(
+      context.req,
+      context.query,
+      context.relation,
+      context.parentModel,
+    );
+
+    // Users should be able to filter records
+    context.queryParser.applyWheres(context.query, context.conditions.q);
+
+    // User should be able to select sorting fields and types
+    context.queryParser.applySorting(context.query, context.conditions.sort);
+
     context.result = await context.query;
   }
 };
