1a9f1216918029e23afe6964e4542752975cdf99
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-08-07 19:37:48 +0200

    Fixed docker compose issues

--- scripts/test-postgres13.sh
+++ scripts/test-postgres13.sh
@@ -3,8 +3,8 @@ set -e
 echo "Postgres-13 Tests have been started."
 
 echo "Setting up Postgres-13 container images"
-docker-compose -f "./tests/integrations/docker-compose.postgres13.yml" down
-docker-compose -f "./tests/integrations/docker-compose.postgres13.yml" up -d --build
+docker compose -f "./tests/integrations/docker-compose.postgres13.yml" down
+docker compose -f "./tests/integrations/docker-compose.postgres13.yml" up -d --build
 
 npm run build
 
@@ -16,4 +16,4 @@ cd ./tests/integrations && npm install && npm run build && node index.js postgre
 
 echo "Downing the database container"
 cd ../../
-docker-compose -f "./tests/integrations/docker-compose.postgres13.yml" down
+docker compose -f "./tests/integrations/docker-compose.postgres13.yml" down
