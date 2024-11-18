1f8f0a0eff1aa4174449ae3eb99ccceda29cbbdc
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2024-11-18 18:21:21 +0100

    Fix

--- src/Phases/List/SerializePhase.ts
+++ src/Phases/List/SerializePhase.ts
@@ -2,15 +2,30 @@ import { filterHiddenFields, serializeData } from "../../Handlers/Helpers";
 import { IContext } from "../../Interfaces";
 
 export default async (context: IContext) => {
-  // Serializing the data by the model's serialize method
-  context.result.data = await serializeData(
-    context.version,
-    context.result?.data || context.result || [],
-    context.model.serialize,
-    context.handlerType,
-    context.req,
-  );
+  const { version, model, handlerType, req } = context;
 
-  // Filtering hidden fields from the response data.
-  filterHiddenFields(context.result.data, context.model.instance.hiddens);
+  if (context.result?.data) {
+    // Serializing the data by the model's serialize method
+    context.result.data = await serializeData(
+      version,
+      context.result?.data || [],
+      model.serialize,
+      handlerType,
+      req,
+    );
+
+    // Filtering hidden fields from the response data.
+    filterHiddenFields(context.result.data, context.model.instance.hiddens);
+  } else if (context.result && Array.isArray(context.result)) {
+    context.result = await serializeData(
+      version,
+      context.result || [],
+      model.serialize,
+      handlerType,
+      req,
+    );
+
+    // Filtering hidden fields from the response data.
+    filterHiddenFields(context.result, context.model.instance.hiddens);
+  }
 };
