45df614ca6f2e9a7ea69f0b3c9ffcbbb424a945a
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2025-06-19 23:26:25 +0200

    fix

--- packages/axe-api/src/definers/useInsertHandler.ts
+++ packages/axe-api/src/definers/useInsertHandler.ts
@@ -6,8 +6,13 @@ export const useInsertHandler = <Model>(resource: Resource<Model>) => {
   type T = ExtractModel<Model>;
 
   return {
-    fillable: (columns: Array<keyof T>) => {
+    fillable(columns: Array<keyof T>) {
       resource.config.fillables = columns;
+      return this;
     },
-  };
+  } as Return<T>;
+};
+
+type Return<T> = {
+  fillable: (columns: Array<keyof T>) => Return<T>;
 };
